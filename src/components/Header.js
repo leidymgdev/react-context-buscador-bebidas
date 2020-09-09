461fb4ea73b4054f06becbf796ee2445c8fa760f
Leidy Johanna Martínez Giraldo <martinezgiraldoleidy@gmail.com>
2020-09-09 02:07:09 -0500

    Carga 100% proyecto con url productiva

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-alert">
+      <h1>Busca receta de bebidas</h1>
+    </header>
+  );
+};
+
+export default Header;
